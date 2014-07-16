1e1e60f04e2ac593b1b0227620ed04362c2fc6d4
Philipp Burgmer <burgmer@weiglewilczek.com>
2014-07-17 00:19:22 +0200

    remove content

--- build-config/project.config.js
+++ build-config/project.config.js
@@ -28,28 +28,18 @@ module.exports = {
   vendor: {
     files: {
       js: [
-        'angular/angular.js',
-        'angular-animate/angular-animate.js',
-        'angular-bootstrap/ui-bootstrap-tpls.js',
-        'angular-growl/build/angular-growl.js',
-        'angular-ui-router/release/angular-ui-router.js',
-        'lodash/dist/lodash.compat.js'
       ],
       js_mock: [
         'angular-mocks/angular-mocks.js'
       ],
       js_spec: [
-        //'lodash/dist/lodash.compat.js'
       ],
       js_e2e: [
-        //'lodash/dist/lodash.compat.js'
       ],
       css: [
-        'animate.css/animate.css',
         'angular-growl/build/angular-growl.min.css'
       ],
       assets: [
-        'font-awesome/fonts/*.*',
         'json3/lib/json3.min.js',
         'respond/dest/respond.min.js',
         'console-polyfill/index.js',
--- src/app/app.js
+++ src/app/app.js
@@ -1,22 +1,9 @@
 'use strict';
 
 angular.module('app', [
-  'ngAnimate',
-  'ui.router',
-  'app.templates',
-  'fabs.boilerplate.route.home'
+  'app.templates'
 ]);
 
-angular.module('app').config(function ($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/home');
-});
-
 angular.module('app').controller('AppCtrl', function ($scope) {
-
-  $scope.$on('$stateChangeSuccess', function (event, toState) {
-    if (angular.isDefined(toState.data) && angular.isDefined(toState.data.pageTitle)) {
-      $scope.pageTitle = toState.data.pageTitle + ' | fabs boilerplate';
-    }
-  });
-
+  $scope.welcomeMessage = 'Hello fabs!';
 });
