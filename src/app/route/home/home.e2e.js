6a7e48411645956cf3cfde0b36c7455a825c1667
Philipp Burgmer <burgmer@weiglewilczek.com>
2014-07-16 15:50:08 +0200

    feat(fabs): update fabs to 0.6.0 and adjusts e2e tests to protractor

--- src/app/route/home/home.e2e.js
+++ src/app/route/home/home.e2e.js
@@ -1,15 +1,36 @@
 'use strict';
 
+// Page Object Pattern to separate knowledge about page structure and test code
+var HomeRoute = function () {
+  this.userName = element(by.model('user.name'));
+  this.welcomeMessage = element(by.css('p.message'));
+
+  this.apiData = element(by.css('div.well.api > p > span.data'));
+
+  this.get = function () {
+    browser.get('/');
+  };
+};
+
 describe('Home route', function () {
+  var page = new HomeRoute();
+
   beforeEach(function () {
-    browser().navigateTo('/');
+    page.get();
   });
 
   describe('bindings and translations example', function () {
-
     it('should print welcome message with name', function () {
-      element('.navbar a').click();
-      expect(element('div.well.bindingAndTranslate p').text()).toContain(', World');
+      page.userName.clear();
+      page.userName.sendKeys('Test');
+      expect(page.welcomeMessage.getText()).toContain(', Test');
+    });
+
+  });
+
+  describe('api example', function () {
+    it('should print some data about w11k', function () {
+      expect(page.apiData.getText()).toEqual('Weigle Wilczek GmbH, Esslingen, Germany');
     });
 
   });
